b6fdd9c38d1df35e9a71ea16306cedc58bcdccf1
Melisa Mary Wanjiku <melisawanjiku93@gmail.com>
2025-06-09 01:32:16 +0300

    Complete arrow functions lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression (not arrow function)
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function: squares a number
+const square = x => x * x;
+
+// Arrow function: adds two numbers
+const add = (a, b) => a + b;
